28a9dbeb19fe10dc773643f3dc8e80d374b202aa
Stefano Zanella <zanella.stefano@gmail.com>
2022-09-22 22:06:28 +0200

    Make managedObject() usable only on vals, not vars.

--- lib/src/main/kotlin/me/stefanozanella/gradle/extension/kotlin/ManagedObject.kt
+++ lib/src/main/kotlin/me/stefanozanella/gradle/extension/kotlin/ManagedObject.kt
@@ -1,7 +1,6 @@
 package me.stefanozanella.gradle.extension.kotlin
 
 import org.gradle.api.model.ObjectFactory
-import java.lang.RuntimeException
 import javax.inject.Inject
 import kotlin.reflect.KProperty
 
@@ -9,8 +8,4 @@ open class ManagedObject<T> @Inject constructor(klass: Class<T>, factory: Object
   private var prop: T = factory.newInstance(klass, *parameters)
 
   operator fun getValue(thisRef: Any?, property: KProperty<*>): T = prop
-
-  operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
-    throw RuntimeException("Managed object cannot be reassigned")
-  }
 }
